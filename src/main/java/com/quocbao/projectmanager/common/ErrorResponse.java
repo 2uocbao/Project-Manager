79178ca3286899330c511e4a4ef961bddcb1df4e
2uocbao2116 <2uocbao.IT@gmail.com>
2024-09-28 12:13:54 +0700

    Update Error Response

--- src/main/java/com/quocbao/projectmanager/common/ErrorResponse.java
+++ src/main/java/com/quocbao/projectmanager/common/ErrorResponse.java
@@ -10,57 +10,9 @@ public class ErrorResponse {
 	@JsonProperty("status")
 	private int status;
 
-	@JsonProperty("error")
-	private Error error;
+	@JsonProperty("title")
+	private String title;
 
-	public ErrorResponse() {
-
-	}
-
-	public ErrorResponse(int status, Error error) {
-		this.status = status;
-		this.error = error;
-	}
-
-	@Setter
-	public class Error {
-
-		@JsonProperty("code")
-		private String code;
-
-		@JsonProperty("message")
-		private String message;
-
-		@JsonProperty("detail")
-		private Detail detail;
-
-		public Error() {
-
-		}
-
-		public Error(String code, String message, Detail detail) {
-			this.code = code;
-			this.message = message;
-			this.detail = detail;
-		}
-
-		@Setter
-		public class Detail {
-
-			@JsonProperty("field")
-			private String field;
-
-			@JsonProperty("issue")
-			private String issue;
-
-			public Detail() {
-
-			}
-
-			public Detail(String field, String issue) {
-				this.field = field;
-				this.issue = issue;
-			}
-		}
-	}
+	@JsonProperty("detail")
+	private String detail;
 }
