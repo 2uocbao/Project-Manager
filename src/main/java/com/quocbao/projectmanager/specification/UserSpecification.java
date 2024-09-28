c97c81d458f8f6b3b444ec69556eee3b27ca621a
2uocbao2116 <2uocbao.IT@gmail.com>
2024-09-28 14:03:45 +0700

    del hasPhoneNumber, add findByColumn

--- src/main/java/com/quocbao/projectmanager/specification/UserSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/UserSpecification.java
@@ -10,6 +10,7 @@ import com.quocbao.projectmanager.entity.User_;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Subquery;
+import jakarta.persistence.metamodel.SingularAttribute;
 
 public class UserSpecification {
 
@@ -17,8 +18,8 @@ public class UserSpecification {
 
 	}
 
-	public static Specification<User> hasPhoneNumber(String phoneNumber) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(User_.phoneNumber), phoneNumber);
+	public static Specification<User> findByColumn(SingularAttribute<User, String> column, String value) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(column), value);
 	}
 
 	public static Specification<User> propertiesByUserId(Long userId) {
@@ -28,7 +29,6 @@ public class UserSpecification {
 			Root<Friend> rootFriend = subquery.from(Friend.class);
 			subquery.select(rootFriend.get(Friend_.friendId));
 			subquery.where(criteriaBuilder.equal(rootFriend.get(Friend_.userId), userId));
-			
 			Predicate predicate = criteriaBuilder.in(root.get(User_.id)).value(subquery);
 			query.where(predicate);
 			return predicate;
