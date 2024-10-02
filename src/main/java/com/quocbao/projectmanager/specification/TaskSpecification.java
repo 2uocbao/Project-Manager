9a97431f920c10c6ba9c9692c38d621f8da33808
2uocbao2116 <2uocbao.IT@gmail.com>
2024-10-02 23:31:04 +0700

    Add new method

--- src/main/java/com/quocbao/projectmanager/specification/TaskSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/TaskSpecification.java
@@ -1,8 +1,37 @@
 package com.quocbao.projectmanager.specification;
 
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.quocbao.projectmanager.entity.Project;
+import com.quocbao.projectmanager.entity.Task;
+import com.quocbao.projectmanager.entity.Task_;
+import com.quocbao.projectmanager.entity.User;
+
 public class TaskSpecification {
 
 	private TaskSpecification() {
 
+	}
+
+	public static Specification<Task> getTaskByUserId(User user) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.user), user);
+	}
+
+	public static Specification<Task> getTaskByProjectId(Project project) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.project), project);
+	}
+
+	public static Specification<Task> getTaskByStatus(String status) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.status), status);
+	}
+
+	public static Specification<Task> getTaskByType(String type) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.type), type);
+	}
+
+	public static Specification<Task> getTaskByDate(LocalDate dateEnd) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get(Task_.dateEnd), dateEnd);
 	}
 }
