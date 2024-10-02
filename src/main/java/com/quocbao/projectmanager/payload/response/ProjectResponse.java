a06124342a38bd819c3f4c0409398aa90adeae60
2uocbao2116 <2uocbao.IT@gmail.com>
2024-10-02 23:17:11 +0700

    Change type date time

--- src/main/java/com/quocbao/projectmanager/payload/response/ProjectResponse.java
+++ src/main/java/com/quocbao/projectmanager/payload/response/ProjectResponse.java
@@ -1,12 +1,15 @@
 package com.quocbao.projectmanager.payload.response;
 
-import java.sql.Timestamp;
+import java.util.Date;
+import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.quocbao.projectmanager.entity.Project;
 
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter
 public class ProjectResponse {
 
@@ -23,10 +26,10 @@ public class ProjectResponse {
 	private String status;
 
 	@JsonProperty("created_at")
-	private Timestamp createdAt;
+	private Date createdAt;
 
 	@JsonProperty("date_end")
-	private Timestamp dateEnd;
+	private LocalDate dateEnd;
 
 	public ProjectResponse() {
 
