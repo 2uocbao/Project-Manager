372898350377526e2571b84e11eb8a0fc800aca6
2uocbao2116 <2uocbao.IT@gmail.com>
2024-10-02 23:27:20 +0700

    Change return type, transmission parameter

--- src/main/java/com/quocbao/projectmanager/service/ProjectService.java
+++ src/main/java/com/quocbao/projectmanager/service/ProjectService.java
@@ -2,23 +2,18 @@ package com.quocbao.projectmanager.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.quocbao.projectmanager.payload.request.ProjectRequest;
 import com.quocbao.projectmanager.payload.response.ProjectResponse;
 
-@Service
 public interface ProjectService {
 
-	public void createProject(ProjectRequest projectRequest);
+	public ProjectResponse createProject(Long userId, ProjectRequest projectRequest);
 
-	public ProjectResponse getProject(Long projectId);
+	public ProjectResponse getProject(Long userId, Long projectId);
 
-	public void updateProject(Long projectId, ProjectRequest projectRequest);
+	public ProjectResponse updateProject(Long projectId, ProjectRequest projectRequest);
 
-	public void deleteProject(Long projectId);
-
-	public List<ProjectResponse> getAllProjects(Long userId);
+	public String deleteProject(Long projectId);
 
 	public List<ProjectResponse> getProjectsByUserIdAndStatus(Long userId, String status);
 }
