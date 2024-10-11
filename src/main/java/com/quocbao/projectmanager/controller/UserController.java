de896a34e7d42a66e34eadb139a2a6bdf86b7332
2uocbao2116 <2uocbao.IT@gmail.com>
2024-10-11 15:35:16 +0700

    hanlde link hateoas, add method pageable get friend

--- src/main/java/com/quocbao/projectmanager/controller/UserController.java
+++ src/main/java/com/quocbao/projectmanager/controller/UserController.java
@@ -1,18 +1,12 @@
 package com.quocbao.projectmanager.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,9 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quocbao.projectmanager.common.DataResponse;
-import com.quocbao.projectmanager.payload.request.FriendRequest;
+import com.quocbao.projectmanager.common.LinkHateoas;
+import com.quocbao.projectmanager.common.PaginationResponse;
 import com.quocbao.projectmanager.payload.request.LoginRequest;
-import com.quocbao.projectmanager.payload.request.ProjectRequest;
 import com.quocbao.projectmanager.payload.request.UserRequest;
 import com.quocbao.projectmanager.payload.response.UserResponse;
 import com.quocbao.projectmanager.service.UserService;
@@ -38,16 +32,19 @@ import jakarta.validation.Valid;
 public class UserController {
 
 	private UserService userService;
+	private LinkHateoas linkHateoas;
 
-	public UserController(UserService userService) {
+	public UserController(UserService userService, LinkHateoas linkHateoas) {
 		this.userService = userService;
+		this.linkHateoas = linkHateoas;
 	}
 
 	@GetMapping()
 	public ResponseEntity<DataResponse> getUser(@RequestParam Long userId) {
 		EntityModel<UserResponse> entityModel = EntityModel.of(userService.getUser(userId));
-		entityModel.add(linkUpdateUser(userId, new UserRequest())).add(linkGetFriendsByUserId(userId))
-				.add(linkCreateProject(userId)).add(linkProjectsByUserId(userId, "INPROGRESS"));
+		entityModel.add(linkHateoas.linkUpdateUser(userId)).add(linkHateoas.linkGetFriendsByUserId(userId))
+				.add(linkHateoas.linkCreateProject(userId)).add(linkHateoas.linkGetProjectsByUserId(userId, "PLANNING"))
+				.add(linkHateoas.linkGetGroupByUserId(userId));
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), entityModel, "User creation request successful."),
 				HttpStatus.OK);
@@ -57,7 +54,7 @@ public class UserController {
 	public ResponseEntity<DataResponse> updateUser(@Valid @PathVariable Long userId,
 			@Valid @RequestBody UserRequest userRequest) {
 		EntityModel<UserResponse> entityModel = EntityModel.of(userService.updateUser(userId, userRequest));
-		entityModel.add(linkGetUser(userId));
+		entityModel.add(linkHateoas.linkGetUser(userId));
 		DataResponse dataResponse = new DataResponse(HttpStatus.OK.value(), entityModel,
 				"User information update successful.");
 		return new ResponseEntity<>(dataResponse, HttpStatus.OK);
@@ -66,9 +63,12 @@ public class UserController {
 	@GetMapping("/login")
 	public ResponseEntity<DataResponse> login(@RequestBody LoginRequest loginRequest) {
 		UserResponse userResponse = userService.loginUser(loginRequest);
-		EntityModel<UserResponse> entityModel = EntityModel.of(userResponse).add(linkGetUser(userResponse.getId()))
-				.add(linkGetFriendsByUserId(userResponse.getId())).add(linkCreateProject(userResponse.getId()))
-				.add(linkProjectsByUserId(userResponse.getId(), "INPROGRESS"));
+		EntityModel<UserResponse> entityModel = EntityModel.of(userResponse)
+				.add(linkHateoas.linkGetUser(userResponse.getId()))
+				.add(linkHateoas.linkGetFriendsByUserId(userResponse.getId()))
+				.add(linkHateoas.linkCreateProject(userResponse.getId()))
+				.add(linkHateoas.linkGetProjectsByUserId(userResponse.getId(), "PLANNING"))
+				.add(linkHateoas.linkGetGroupByUserId(userResponse.getId()));
 		return new ResponseEntity<>(new DataResponse(HttpStatus.ACCEPTED.value(), entityModel, "Login successful."),
 				HttpStatus.ACCEPTED);
 	}
@@ -77,9 +77,10 @@ public class UserController {
 	public ResponseEntity<DataResponse> register(@Valid @RequestBody UserRequest userRequest) {
 		UserResponse userResponse = userService.registerUser(userRequest);
 		EntityModel<UserResponse> entityModel = EntityModel.of(userResponse);
-		entityModel.add(linkGetUser(userResponse.getId())).add(linkGetFriendsByUserId(userResponse.getId()))
-				.add(linkCreateProject(userResponse.getId()))
-				.add(linkProjectsByUserId(userResponse.getId(), "INPROGRESS"));
+		entityModel.add(linkHateoas.linkGetUser(userResponse.getId()))
+				.add(linkHateoas.linkGetFriendsByUserId(userResponse.getId()))
+				.add(linkHateoas.linkCreateProject(userResponse.getId()))
+				.add(linkHateoas.linkGetProjectsByUserId(userResponse.getId(), "PLANNING"));
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), entityModel, "Account creation request successful."),
 				HttpStatus.OK);
@@ -99,45 +100,17 @@ public class UserController {
 	}
 
 	@GetMapping("/{userId}/friends")
-	public ResponseEntity<DataResponse> getFriendsByUserId(@PathVariable Long userId) {
-		List<EntityModel<UserResponse>> entityModels = userService.getFriendsByUserId(userId).stream()
-				.map(userResponse -> EntityModel.of(userResponse).add(linkGetUser(userResponse.getId()))).toList();
-		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), entityModels, "Friends of user."),
-				HttpStatus.OK);
-	}
-
-	@MessageMapping("/friend.request")
-	@SendTo("/topic/{userId}/friend-request")
-	public ResponseEntity<DataResponse> friendRequest(@DestinationVariable Long userId,
-			@Payload FriendRequest friendRequest) {
-		DataResponse dataResponse = new DataResponse(HttpStatus.OK.value(),
-				"You have friend request from " + friendRequest.getFromUserName() + " " + friendRequest.getFromUser(),
-				"Notification");
-		return new ResponseEntity<>(dataResponse, HttpStatus.OK);
-	}
-
-	private Link linkGetUser(Long userId) {
-		return linkTo(methodOn(UserController.class).getUser(userId)).withRel("get")
-				.withTitle("View user requested information").withType("GET");
-	}
-
-	private Link linkUpdateUser(Long userId, UserRequest userRequest) {
-		return linkTo(methodOn(UserController.class).updateUser(userId, userRequest)).withRel("update")
-				.withTitle("Update this user").withType("PUT");
-	}
-
-	private Link linkGetFriendsByUserId(Long userId) {
-		return linkTo(methodOn(UserController.class).getFriendsByUserId(userId)).withRel("get")
-				.withTitle("Get list friends of user by user id").withType("GET");
-	}
-
-	private Link linkProjectsByUserId(Long userId, String status) {
-		return linkTo(methodOn(ProjectController.class).getProjectsByStatus(userId, status)).withRel("projects")
-				.withTitle("Get list projects by user id").withType("GET");
-	}
-
-	private Link linkCreateProject(Long userId) {
-		return linkTo(methodOn(ProjectController.class).createProject(userId, new ProjectRequest())).withRel("projects")
-				.withTitle("Create a new project.").withType("POST");
+	public ResponseEntity<PaginationResponse<EntityModel<UserResponse>>> getFriendsByUserId(@PathVariable Long userId,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+		Page<UserResponse> userResponsePage = userService.getFriendsByUserId(userId, PageRequest.of(page, size));
+		List<EntityModel<UserResponse>> entityModels = userResponsePage.getContent().stream()
+				.map(userResponse -> EntityModel.of(userResponse).add(linkHateoas.linkGetUser(userResponse.getId())))
+				.toList();
+		PaginationResponse<EntityModel<UserResponse>> paginationResponse = new PaginationResponse<>(HttpStatus.OK,
+				entityModels, userResponsePage.getPageable().getPageNumber(), userResponsePage.getSize(),
+				userResponsePage.getTotalElements(), userResponsePage.getTotalPages(),
+				userResponsePage.getSort().isSorted(), userResponsePage.getSort().isUnsorted(),
+				userResponsePage.getSort().isEmpty());
+		return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
 	}
 }
