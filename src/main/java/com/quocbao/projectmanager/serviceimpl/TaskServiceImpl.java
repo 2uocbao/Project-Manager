527f0979b29447f14524ddaed453eaddfb74be10
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:25:26 +0700

    Update handling in methods

--- src/main/java/com/quocbao/projectmanager/serviceimpl/TaskServiceImpl.java
+++ src/main/java/com/quocbao/projectmanager/serviceimpl/TaskServiceImpl.java
@@ -1,7 +1,7 @@
 package com.quocbao.projectmanager.serviceimpl;
 
-import java.time.LocalDate;
 import java.util.Objects;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +9,12 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.quocbao.projectmanager.common.MethodGeneral;
+import com.quocbao.projectmanager.common.StatusEnum;
 import com.quocbao.projectmanager.entity.Project;
 import com.quocbao.projectmanager.entity.Task;
 import com.quocbao.projectmanager.entity.User;
 import com.quocbao.projectmanager.exception.ResourceNotFoundException;
+import com.quocbao.projectmanager.payload.request.NotifiRequest;
 import com.quocbao.projectmanager.payload.request.TaskRequest;
 import com.quocbao.projectmanager.payload.response.TaskResponse;
 import com.quocbao.projectmanager.repository.ProjectRepository;
@@ -41,12 +43,10 @@ public class TaskServiceImpl implements TaskService {
 	}
 
 	@Override
-	public TaskResponse createTask(Long projectId, TaskRequest taskRequest) {
+	public TaskResponse createTask(UUID projectId, TaskRequest taskRequest) {
 		Project project = projectRepository.findById(projectId)
 				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-
 		methodGeneral.validatePermission(taskRequest.getUserId(), project.getUser().getId());
-
 		Task task = new Task(taskRequest);
 		task.setProject(project);
 		task.setUser(project.getUser());
@@ -54,59 +54,38 @@ public class TaskServiceImpl implements TaskService {
 	}
 
 	@Override
-	public Task getTask(Long taskId) {
-		return taskRepository.findById(taskId).map(t -> t)
-				.orElseThrow(() -> new ResourceNotFoundException("Task information can not found"));
+	public TaskResponse getTask(UUID taskId) {
+		Task task = taskRepository.findOne(Specification.where(TaskSpecification.getTaskDetailCustom(taskId))).get();
+		TaskResponse taskResponse = new TaskResponse(task);
+		taskResponse.setUserId(task.getUser().getId());
+		taskResponse.setUsername(task.getUser().getFirstName() + " " + task.getUser().getLastName());
+		return taskResponse;
 	}
 
 	@Override
-	public TaskResponse updateTask(Long taskId, Long userId, TaskRequest taskRequest) {
+	public TaskResponse updateTask(UUID taskId, UUID userId, TaskRequest taskRequest) {
 		return taskRepository.findById(taskId).map(t -> {
-
-			methodGeneral.validatePermission(userId, t.getProject().getUser().getId());
-
-			// If true, check if admin is assigning task to member or updating task
-			if (isTaskAssignedToNewUser(t, userId)) {
-				assignTaskToNewUser(t, taskRequest);
-				updateStatusProject(t.getProject());
+			if (Boolean.FALSE.equals(isAdminProject(userId, t.getProject().getUser().getId()))) {
+				t.setContentSubmit(taskRequest.getContentSubmit());
+				return new TaskResponse(taskRepository.save(t));
 			}
-
+			t.updateTask(taskRequest);
 			return new TaskResponse(taskRepository.save(t));
 		}).orElseThrow(() -> new ResourceNotFoundException("Task not found for update."));
 	}
 
-	private boolean isTaskAssignedToNewUser(Task task, Long userId) {
-		return !Objects.equals(task.getUser().getId(), userId);
-	}
-
-	private void assignTaskToNewUser(Task task, TaskRequest taskRequest) {
-		task.updateTask(taskRequest);
-		task.setUser(User.builder().id(taskRequest.getUserId()).build());
-		task.setStatus("WORKING");
-		// Send push notification to the assigned user.
-		pushNotificationService.pushNotification(taskRequest.getUserId(),
-				"/topic/" + taskRequest.getUserId().toString() + "/notification",
-				"You have a new task from project: " + task.getProject().getName(), "New Task");
-	}
-
-	private void updateStatusProject(Project project) {
-		project.setStatus("INPROGRESS");
-		projectRepository.save(project);
-	}
-
 	@Override
-	public TaskResponse commitContentTask(Long taskId, TaskRequest taskRequest) {
+	public TaskResponse commitContentTask(UUID taskId, TaskRequest taskRequest) {
 		return taskRepository.findById(taskId).map(t -> {
-
 			methodGeneral.validatePermission(taskRequest.getUserId(), t.getUser().getId());
-
 			t.setContentSubmit(taskRequest.getContentSubmit());
+			t.setStatus(StatusEnum.DONE);
 			return new TaskResponse(taskRepository.save(t));
 		}).orElseThrow(() -> new ResourceNotFoundException("Not found task need update."));
 	}
 
 	@Override
-	public String deleteTask(Long userId, Long taskId) {
+	public String deleteTask(UUID userId, UUID taskId) {
 		return taskRepository.findById(taskId).map(t -> {
 			methodGeneral.validatePermission(userId, t.getProject().getUser().getId());
 			taskRepository.delete(Task.builder().id(taskId).build());
@@ -115,33 +94,75 @@ public class TaskServiceImpl implements TaskService {
 	}
 
 	@Override
-	public Page<TaskResponse> getTaskByUserIdAndProjectIdAndStatusAndDate(Long userId, Long projectId, String status,
-			LocalDate date, Pageable pageable) {
-		Project project = projectRepository.findById(projectId)
-				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-
-		if (Boolean.TRUE.equals(isAdminProject(userId, project))) {
-			return listTasksByAdminProject(project, status, pageable).map(TaskResponse::new);
+	public Page<TaskResponse> getTasks(UUID userId, UUID projectId, String status, Pageable pageable) {
+		Project project = projectRepository.findById(projectId).get();
+		if (Boolean.TRUE.equals(isAdminProject(userId, project.getUser().getId()))) {
+			return listTasksByAdminProject(projectId, status, pageable).map(TaskResponse::new);
 		}
-
-		return listTasksByUser(userId, projectId, status, date, pageable).map(TaskResponse::new);
+		return listTasksByUser(userId, projectId, status, pageable).map(TaskResponse::new);
 	}
 
-	private Boolean isAdminProject(Long userId, Project project) {
-		return Objects.equals(userId, project.getUser().getId());
+	@Override
+	public String getGDPTask(UUID userId, UUID projectId) {
+		Specification<Task> spec;
+		spec = byStatus(userId, projectId, StatusEnum.DONE);
+		long done = taskRepository.count(spec);
+		spec = byStatus(userId, projectId, StatusEnum.PENDING);
+		long pending = taskRepository.count(spec);
+		spec = byStatus(userId, projectId, StatusEnum.INPROGRESS);
+		long todo = taskRepository.count(spec);
+		return "Done: " + done + "pending: " + pending + "todo: " + todo;
 	}
 
-	private Page<Task> listTasksByAdminProject(Project project, String status, Pageable pageable) {
+	@Override
+	public void assignToUser(UUID taskId, UUID userId, UUID toUserId) {
+		taskRepository.findById(taskId).ifPresent(task -> {
+			methodGeneral.validatePermission(userId, task.getProject().getUser().getId());
+			if (userId.equals(toUserId)) {
+				task.setUser(User.builder().id(userId).build());
+				task.setStatus(StatusEnum.PENDING);
+				taskRepository.save(task);
+			}
+			assignTaskToNewUser(task, toUserId);
+		});
+	}
+
+	private Specification<Task> byStatus(UUID userId, UUID projectId, StatusEnum status) {
+		UUID userIdAdminProject = projectRepository.findById(projectId).get().getUser().getId();
+		if (Boolean.TRUE.equals(isAdminProject(userId, userIdAdminProject))) {
+			return Specification.where(TaskSpecification.getTaskByProjectId(projectId))
+					.and(TaskSpecification.getTaskByStatus(status.toString()));
+		}
+		return Specification.where(TaskSpecification.getTaskByUserId(userId))
+				.and(TaskSpecification.getTaskByProjectId(projectId)).and(TaskSpecification.getTaskByStatus(status.toString()));
+	}
+
+	private Task assignTaskToNewUser(Task task, UUID toUserId) {
+		task.setUser(User.builder().id(toUserId).build());
+		task.setStatus(StatusEnum.INPROGRESS);
+		taskRepository.save(task);
+		// Send notification to the assigned user.
+		pushNotificationService.pushNotification(NotifiRequest.builder().receiver(toUserId).type("PROJECT")
+				.from(task.getProject().getName()).sender(task.getProject().getId()).build());
+		return task;
+	}
+
+	private Boolean isAdminProject(UUID userId, UUID project) {
+		return Objects.equals(userId, project);
+	}
+
+	private Page<Task> listTasksByAdminProject(UUID project, String status, Pageable pageable) {
 		Specification<Task> spec = Specification.where(TaskSpecification.getTaskByProjectId(project))
 				.and(TaskSpecification.getTaskByStatus(status));
 		return taskRepository.findAll(spec, pageable);
 	}
 
-	private Page<Task> listTasksByUser(Long userId, Long projectId, String status, LocalDate date, Pageable pageable) {
-		Specification<Task> spec = Specification
-				.where(TaskSpecification.getTaskByUserId(User.builder().id(userId).build())
-						.and(TaskSpecification.getTaskByProjectId(Project.builder().id(projectId).build()))
-						.and(TaskSpecification.getTaskByStatus(status)).and(TaskSpecification.getTaskByDate(date)));
+	private Page<Task> listTasksByUser(UUID userId, UUID projectId, String status, Pageable pageable) {
+		Specification<Task> spec;
+
+		spec = Specification.where(TaskSpecification.getTaskByUserId(userId)
+				.and(TaskSpecification.getTaskByProjectId(projectId)).and(TaskSpecification.getTaskByStatus(status)));
+
 		return taskRepository.findAll(spec, pageable);
 	}
 }
