a814c0433580b9a6327f783c1c20bc90f454441b
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:10:29 +0700

    Change type response and parameters

--- src/main/java/com/quocbao/projectmanager/service/ProjectService.java
+++ src/main/java/com/quocbao/projectmanager/service/ProjectService.java
@@ -1,25 +1,29 @@
 package com.quocbao.projectmanager.service;
 
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.quocbao.projectmanager.elasticsearch.entity.ProjectES;
+//import com.quocbao.projectmanager.elasticsearch.entity.ProjectES;
 import com.quocbao.projectmanager.payload.request.ProjectRequest;
 import com.quocbao.projectmanager.payload.response.ProjectResponse;
 
 public interface ProjectService {
 
-	public ProjectResponse createProject(Long userId, ProjectRequest projectRequest);
+	public ProjectResponse createProject(UUID userId, ProjectRequest projectRequest);
 
-	public ProjectResponse getProject(Long userId, Long projectId);
+	public ProjectResponse getProject(UUID userId, UUID projectId);
 
-	public ProjectResponse updateProject(Long userId, Long projectId, ProjectRequest projectRequest);
+	public ProjectResponse updateProject(UUID userId, UUID projectId, ProjectRequest projectRequest);
 
-	public String deleteProject(Long userId, Long projectId);
+	public String deleteProject(UUID userId, UUID projectId);
 
-	public Page<ProjectResponse> getProjectsByUserIdAndStatus(Long userId, String status, String keySearch,
+	public Page<ProjectResponse> getProjects(UUID userId, String status, String startDate, String endDate,
 			Pageable pageable);
 
-	public Page<ProjectES> findByUserIdAndStatusAndKeySearch(Long userId, String status, String keySearch,
-			Pageable pageable);
+//	public Page<String> findByUserIdAndStatusAndKeySearch(UUID userId, String status, String keySearch,
+//			Pageable pageable);
+
+	public String getGDPProjectStatus(UUID userId, String startDate, String endDate);
 }
