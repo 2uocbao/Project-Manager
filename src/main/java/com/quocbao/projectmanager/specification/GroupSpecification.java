b51d9976d74346d54a04321c70c392bffa2b6eb7
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:29:47 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/specification/GroupSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/GroupSpecification.java
@@ -4,11 +4,6 @@ import org.springframework.data.jpa.domain.Specification;
 
 import com.quocbao.projectmanager.entity.Group;
 import com.quocbao.projectmanager.entity.Group_;
-import com.quocbao.projectmanager.entity.Project_;
-import com.quocbao.projectmanager.entity.User;
-import com.quocbao.projectmanager.entity.User_;
-
-import jakarta.persistence.criteria.Join;
 
 public class GroupSpecification {
 
@@ -16,14 +11,7 @@ public class GroupSpecification {
 
 	}
 
-	public static Specification<Group> getGroupsByUserId(User user) {
-		return (root, query, criteriaBuilder) -> {
-			Join<Group, User> member = root.join(Group_.users);
-			return criteriaBuilder.equal(member.get(User_.id), user.getId());
-		};
-	}
-
 	public static Specification<Group> search(String keySearch) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Project_.name), keySearch);
+		return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Group_.name), keySearch);
 	}
 }
