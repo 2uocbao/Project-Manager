31e51edaa104a401f964ca4dbf40c1d21736e918
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 13:56:33 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/repository/ProjectRepository.java
+++ src/main/java/com/quocbao/projectmanager/repository/ProjectRepository.java
@@ -1,5 +1,7 @@
 package com.quocbao.projectmanager.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -7,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import com.quocbao.projectmanager.entity.Project;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
+public interface ProjectRepository extends JpaRepository<Project, UUID>, JpaSpecificationExecutor<Project> {
 
 }
