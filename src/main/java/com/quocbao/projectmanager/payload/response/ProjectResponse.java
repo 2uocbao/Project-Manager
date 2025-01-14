c46d01c837addebdaa77de3c2625cda0c1bdc33b
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 13:37:03 +0700

    Change type properties

--- src/main/java/com/quocbao/projectmanager/payload/response/ProjectResponse.java
+++ src/main/java/com/quocbao/projectmanager/payload/response/ProjectResponse.java
@@ -1,9 +1,9 @@
 package com.quocbao.projectmanager.payload.response;
 
-import java.util.Date;
-import java.time.LocalDate;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.quocbao.projectmanager.common.ConvertData;
 import com.quocbao.projectmanager.entity.Project;
 
 import lombok.Getter;
@@ -14,7 +14,10 @@ import lombok.Setter;
 public class ProjectResponse {
 
 	@JsonProperty("id")
-	private Long id;
+	private UUID id;
+	
+	@JsonProperty("user_id")
+	private UUID userId;
 
 	@JsonProperty("name")
 	private String name;
@@ -22,14 +25,17 @@ public class ProjectResponse {
 	@JsonProperty("description")
 	private String description;
 
+	@JsonProperty("tasks_complete")
+	private double tasks;
+
 	@JsonProperty("status")
 	private String status;
 
 	@JsonProperty("created_at")
-	private Date createdAt;
+	private String createdAt;
 
 	@JsonProperty("date_end")
-	private LocalDate dateEnd;
+	private String dateEnd;
 
 	public ProjectResponse() {
 
@@ -37,10 +43,12 @@ public class ProjectResponse {
 
 	public ProjectResponse(Project project) {
 		this.id = project.getId();
+		this.userId = project.getUser().getId();
 		this.name = project.getName();
 		this.description = project.getDescription();
-		this.status = project.getStatus();
-		this.createdAt = project.getCreatedAt();
-		this.dateEnd = project.getDateEnd();
+		this.status = project.getStatus().toString();
+		this.createdAt = ConvertData.timeStampToString(project.getCreatedAt());
+		this.dateEnd = ConvertData.timeStampToString(project.getDateEnd());
+		this.tasks = project.getPerTaskComplete();
 	}
 }
