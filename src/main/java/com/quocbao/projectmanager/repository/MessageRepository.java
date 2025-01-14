d21bd4f173a66b21fa447247a7c8100b583a43b3
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 13:51:37 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/repository/MessageRepository.java
+++ src/main/java/com/quocbao/projectmanager/repository/MessageRepository.java
@@ -1,10 +1,14 @@
 package com.quocbao.projectmanager.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.quocbao.projectmanager.entity.Message;
 
-public interface MessageRepository extends JpaRepository<Message, Long>, JpaSpecificationExecutor<Message> {
+@Repository
+public interface MessageRepository extends JpaRepository<Message, UUID>, JpaSpecificationExecutor<Message> {
 
 }
