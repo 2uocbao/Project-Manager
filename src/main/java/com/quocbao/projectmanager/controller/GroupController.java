da00a1993546f851614e77e13546c997da3f8961
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 10:49:50 +0700

    Change and add

--- src/main/java/com/quocbao/projectmanager/controller/GroupController.java
+++ src/main/java/com/quocbao/projectmanager/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.quocbao.projectmanager.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +21,7 @@ import com.quocbao.projectmanager.common.DataResponse;
 import com.quocbao.projectmanager.common.LinkHateoas;
 import com.quocbao.projectmanager.common.PaginationResponse;
 import com.quocbao.projectmanager.entity.Group;
+import com.quocbao.projectmanager.payload.response.GroupResponse;
 import com.quocbao.projectmanager.payload.response.UserResponse;
 import com.quocbao.projectmanager.service.GroupService;
 
@@ -36,36 +38,35 @@ public class GroupController {
 	}
 
 	@PostMapping("/groups")
-	public ResponseEntity<DataResponse> createGroup(@PathVariable Long userId, @RequestParam String name) {
-		Group group = groupService.createGroup(userId, name);
+	public ResponseEntity<DataResponse> createGroup(@PathVariable UUID userId, @RequestParam UUID withUser) {
+		Group group = groupService.createGroup(userId, withUser);
 		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), group, "Group creation successful."),
 				HttpStatus.OK);
 	}
 
 	@GetMapping("/groups")
-	public ResponseEntity<PaginationResponse<EntityModel<Group>>> getGroupsByUserId(@PathVariable Long userId,
+	public ResponseEntity<PaginationResponse<EntityModel<GroupResponse>>> getGroupsByUserId(@PathVariable UUID userId,
 			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
 			@RequestParam(required = false) String search) {
-		Page<Group> pageGroup = groupService.getGroupsByUserId(userId, PageRequest.of(page, size), search);
-		List<EntityModel<Group>> entityModel = pageGroup.getContent().stream().map(t -> EntityModel.of(t)
-				.add(linkHateoas.linkGetMembersInGroup(t.getId())).add(linkHateoas.linkGetMembersInGroup(t.getId())))
-				.toList();
-		PaginationResponse<EntityModel<Group>> paginationResponse = new PaginationResponse<>(HttpStatus.OK, entityModel,
-				pageGroup.getPageable().getPageNumber(), pageGroup.getSize(), pageGroup.getTotalElements(),
+		Page<GroupResponse> pageGroup = groupService.getGroupsByUserId(userId, PageRequest.of(0, 10), null);
+		List<EntityModel<GroupResponse>> entityModel = pageGroup.stream()
+				.map(t -> EntityModel.of(t).add(linkHateoas.linkGetMessageInGroup(t.getId()))).toList();
+		PaginationResponse<EntityModel<GroupResponse>> paginationResponse = new PaginationResponse<>(HttpStatus.OK,
+				entityModel, pageGroup.getPageable().getPageNumber(), pageGroup.getSize(), pageGroup.getTotalElements(),
 				pageGroup.getTotalPages(), pageGroup.getSort().isSorted(), pageGroup.getSort().isUnsorted(),
 				pageGroup.getSort().isEmpty());
 		return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/groups/{groupId}")
-	public ResponseEntity<DataResponse> deleteGroup(@PathVariable Long userId, @PathVariable Long groupId) {
+	public ResponseEntity<DataResponse> deleteGroup(@PathVariable UUID userId, @PathVariable UUID groupId) {
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), null, groupService.deleteGroup(userId, groupId)),
 				HttpStatus.OK);
 	}
 
 	@PutMapping("/groups/{groupId}")
-	public ResponseEntity<DataResponse> updateGroup(@PathVariable Long userId, @PathVariable Long groupId,
+	public ResponseEntity<DataResponse> updateGroup(@PathVariable UUID userId, @PathVariable UUID groupId,
 			@RequestParam String name) {
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), groupService.updateGroup(userId, groupId, name), "Successful"),
@@ -73,9 +74,9 @@ public class GroupController {
 	}
 
 	@GetMapping("/groups/{groupId}")
-	public ResponseEntity<PaginationResponse<EntityModel<UserResponse>>> getUsersInGroup(@PathVariable Long groupId,
-			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
-			@RequestParam(required = false) String search) {
+	public ResponseEntity<PaginationResponse<EntityModel<UserResponse>>> getUsersInGroup(@PathVariable UUID userId,
+			@PathVariable UUID groupId, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size, @RequestParam(required = false) String search) {
 		Page<UserResponse> userResponse = groupService.getUsersInGroup(groupId, PageRequest.of(page, size), search);
 		List<EntityModel<UserResponse>> entityModels = userResponse.getContent().stream()
 				.map(t -> EntityModel.of(t).add(linkHateoas.linkGetUser(t.getId()))).toList();
@@ -87,18 +88,31 @@ public class GroupController {
 	}
 
 	@PostMapping("/groups/{groupId}/add")
-	public ResponseEntity<DataResponse> addMember(@PathVariable Long userId, @PathVariable Long groupId,
-			@RequestParam Long memberId) {
+	public ResponseEntity<DataResponse> addMember(@PathVariable UUID userId, @PathVariable UUID groupId,
+			@RequestParam UUID memberId) {
 		return new ResponseEntity<>(
-				new DataResponse(HttpStatus.OK.value(), null, groupService.addUserToGroup(memberId, groupId)),
+				new DataResponse(HttpStatus.OK.value(), null, groupService.addUserToGroup(userId, memberId, groupId)),
 				HttpStatus.OK);
 	}
 
 	@DeleteMapping("/groups/{groupId}/del")
-	public ResponseEntity<DataResponse> delMember(@PathVariable Long userId, @PathVariable Long groupId,
-			@RequestParam Long memberId) {
-		return new ResponseEntity<>(
-				new DataResponse(HttpStatus.OK.value(), null, groupService.removeUserInGroup(memberId, groupId)),
-				HttpStatus.OK);
+	public ResponseEntity<DataResponse> delMember(@PathVariable UUID userId, @PathVariable UUID groupId,
+			@RequestParam UUID memberId) {
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), null,
+				groupService.removeUserInGroup(userId, memberId, groupId)), HttpStatus.OK);
+	}
+
+	@GetMapping("/friends")
+	public ResponseEntity<PaginationResponse<EntityModel<UserResponse>>> getFriendsOfUser(@PathVariable UUID userId,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
+			@RequestParam(defaultValue = "null") String keySearch) {
+		Page<UserResponse> usersPage = groupService.getUsersConnect(userId, PageRequest.of(page, size), keySearch);
+		List<EntityModel<UserResponse>> entityModels = usersPage.getContent().stream().map(EntityModel::of)
+				.toList();
+		PaginationResponse<EntityModel<UserResponse>> paginationResponse = new PaginationResponse<>(HttpStatus.OK,
+				entityModels, usersPage.getNumber(), usersPage.getTotalPages(), usersPage.getTotalElements(),
+				usersPage.getSize(), usersPage.getSort().isSorted(), usersPage.getSort().isUnsorted(),
+				usersPage.getSort().isEmpty());
+		return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
 	}
 }
