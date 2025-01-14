722e823dded07e9ba1e6fdbdf9ca4ff01114f339
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 11:00:48 +0700

    Update properties

--- src/main/java/com/quocbao/projectmanager/entity/Group.java
+++ src/main/java/com/quocbao/projectmanager/entity/Group.java
@@ -1,22 +1,20 @@
 package com.quocbao.projectmanager.entity;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -39,8 +37,8 @@ public class Group implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
 
 	@Column(name = "name")
 	private String name;
@@ -48,21 +46,20 @@ public class Group implements Serializable {
 	@CreationTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "created_at")
-	private Date createdAt;
+	private LocalDateTime createdAt;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+	@OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+	private List<Message> messages;
 
-	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.DETACH)
-	@JoinTable(name = "member_group", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "group_id"))
-	private List<User> users;
+	@OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+	private Set<Member> members;
 
 	public Group() {
 
 	}
 
-	public Group(String name) {
+	public Group(String name, Set<Member> members) {
 		this.name = name;
+		this.members = members;
 	}
 }
