0578d0d07fdac339f87c4f41164f2c200e96e00c
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 10:31:22 +0700

    To AbstactClass\

--- src/main/java/com/quocbao/projectmanager/common/ConvertData.java
+++ src/main/java/com/quocbao/projectmanager/common/ConvertData.java
@@ -1,24 +1,42 @@
 package com.quocbao.projectmanager.common;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-public class ConvertData {
+public abstract class ConvertData {
 
-	public ConvertData() {
-
+	ConvertData() {
 	}
 
-	public LocalDate toDate(String dateString) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Define the date format
-		return LocalDate.parse(dateString, formatter);
+	public static String timeStampToString(Timestamp timestamp) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		return timestamp.toLocalDateTime().format(formatter);
 	}
 
-	public String toLocalDate(Date date) {
+	public static Timestamp toTimestamp(String dateString) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm"); // Define the date format
+		try {
+			Date parseDate = formatter.parse(dateString);
+			return new Timestamp(parseDate.getTime());
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String toLocalDate(Date date) {
 		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()) // or specify a specific zone
 				.toLocalDate();
-		return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-d"));
+	}
+
+	public static String formatHexToUUID(String hex) {
+		return hex.replaceFirst("([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{12})",
+				"$1-$2-$3-$4-$5");
 	}
 }
