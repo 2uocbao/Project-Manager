b161e3637e57fca34c86add9f76cd1e6c4907a50
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 11:03:04 +0700

    Update type properties and add new properties

--- src/main/java/com/quocbao/projectmanager/entity/Notification.java
+++ src/main/java/com/quocbao/projectmanager/entity/Notification.java
@@ -2,20 +2,32 @@ package com.quocbao.projectmanager.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.quocbao.projectmanager.payload.request.NotifiRequest;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
 @Table(name = "notification")
+@Getter
+@Builder
+@AllArgsConstructor
 public class Notification implements Serializable {
 
 	/**
@@ -23,21 +35,26 @@ public class Notification implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
 
 	@Column(name = "user_id")
-	private Long recipientId;
-
-	@Column(name = "message")
-	private String message;
+	private UUID recipientId;
 
 	@Column(name = "status")
 	private boolean read;
 
 	@Column(name = "type")
 	private String type;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "project_id", nullable = true)
+	private Project project;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "sender_id", nullable = true)
+	private User user;
 
 	@CreationTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
@@ -47,11 +64,15 @@ public class Notification implements Serializable {
 	public Notification() {
 
 	}
-
-	public Notification(Long recipientId, String message, String type) {
-		this.recipientId = recipientId;
-		this.message = message;
-		this.type = type;
+	
+	public Notification(NotifiRequest notifiRequest) {
+		this.recipientId = notifiRequest.getReceiver();
+		this.type = notifiRequest.getType();
+		if (notifiRequest.getType().equals("PROJECT")) {
+			this.project = Project.builder().id(notifiRequest.getSender()).build();
+		} else {
+			this.user = User.builder().id(notifiRequest.getSender()).build();
+		}
 		this.read = false;
 	}
 }
