af5355a6be77c8c1c4f024078acf5726733daa03
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:48:45 +0700

    Add group response class

---
+++ src/main/java/com/quocbao/projectmanager/payload/response/GroupResponse.java
@@ -0,0 +1,42 @@
+package com.quocbao.projectmanager.payload.response;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.quocbao.projectmanager.common.ConvertData;
+import com.quocbao.projectmanager.entity.Group;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class GroupResponse {
+
+	@JsonProperty("id")
+	private UUID id;
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("lastMessage")
+	private String lastMessage;
+
+	@JsonProperty("time")
+	private Timestamp time;
+
+	public GroupResponse(String id, String name, String message, Timestamp timeMessage) {
+		this.id = UUID.fromString(ConvertData.formatHexToUUID(id));
+		this.name = name;
+		this.lastMessage = message;
+		this.time = timeMessage;
+	}
+
+	public GroupResponse(Group group) {
+		id = group.getId();
+		name = group.getName();
+//		time = group.getCreatedAt();
+	}
+
+}
