556fee8c8a02bece64d29ea5f198bde4f2f02b64
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 10:56:37 +0700

    Update and add methods

--- src/main/java/com/quocbao/projectmanager/controller/TaskController.java
+++ src/main/java/com/quocbao/projectmanager/controller/TaskController.java
@@ -1,10 +1,12 @@
 package com.quocbao.projectmanager.controller;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.quocbao.projectmanager.common.ConvertData;
 import com.quocbao.projectmanager.common.DataResponse;
 import com.quocbao.projectmanager.common.LinkHateoas;
 import com.quocbao.projectmanager.common.PaginationResponse;
-import com.quocbao.projectmanager.entity.Task;
 import com.quocbao.projectmanager.payload.request.TaskRequest;
 import com.quocbao.projectmanager.payload.response.TaskResponse;
 import com.quocbao.projectmanager.service.TaskService;
@@ -39,43 +39,43 @@ public class TaskController {
 		this.linkHateoas = linkHateoas;
 	}
 
-	@PostMapping("/task")
-	public ResponseEntity<DataResponse> createTask(@PathVariable Long projectId, @RequestBody TaskRequest taskRequest) {
+	@PostMapping("/tasks")
+	public ResponseEntity<DataResponse> createTask(@PathVariable UUID projectId, @RequestBody TaskRequest taskRequest) {
 		TaskResponse taskResponse = taskService.createTask(projectId, taskRequest);
 		EntityModel<TaskResponse> entityModel = EntityModel.of(taskResponse);
-		entityModel.add(linkHateoas.linkGetTask(taskResponse.getId(), projectId)).add(linkHateoas
+		entityModel.add(linkHateoas.linkGetTask(projectId, taskResponse.getId())).add(linkHateoas
 				.linkGetTasksByUserIdAndProjectIdAndStatusAndDate(taskRequest.getUserId(), projectId, "PLANNING"));
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), entityModel, "Task creation request successful."),
 				HttpStatus.OK);
 	}
 
-	@GetMapping("/task/{taskId}")
-	public ResponseEntity<DataResponse> getTask(@PathVariable Long projectId, @PathVariable Long taskId) {
-		Task task = taskService.getTask(taskId);
-		Long userId = task.getUser().getId();
-		TaskResponse taskResponse = new TaskResponse(task);
+	@GetMapping("/tasks/{taskId}")
+	public ResponseEntity<DataResponse> getTask(@PathVariable UUID projectId, @PathVariable UUID taskId) {
+
+		TaskResponse taskResponse = taskService.getTask(taskId);
+
 		EntityModel<TaskResponse> entityModel = EntityModel.of(taskResponse);
-		entityModel.add(linkHateoas.linkUpdateTask(projectId, taskId, userId))
-				.add(linkHateoas.linkDeleteTask(projectId, task.getUser().getId(), taskId))
-				.add(linkHateoas.linkGetUser(userId)).add(linkHateoas.linkCommitContent(projectId, taskId));
+		entityModel.add(linkHateoas.linkUpdateTask(projectId, taskId, taskResponse.getUserId()))
+				.add(linkHateoas.linkDeleteTask(projectId, taskResponse.getUserId(), taskId))
+				.add(linkHateoas.linkGetUser(taskResponse.getUserId()))
+				.add(linkHateoas.linkCommitContent(projectId, taskId));
 		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), entityModel, "Retrieve info Task."),
 				HttpStatus.OK);
 	}
 
-	@PutMapping("/users/{userId}/task/{taskId}")
-	public ResponseEntity<DataResponse> updateTask(@PathVariable Long projectId, @PathVariable Long taskId,
-			@PathVariable Long userId, @RequestBody TaskRequest taskRequest) {
+	@PutMapping("/users/{userId}/tasks/{taskId}")
+	public ResponseEntity<DataResponse> updateTask(@PathVariable UUID projectId, @PathVariable UUID taskId,
+			@PathVariable UUID userId, @RequestBody TaskRequest taskRequest) {
 		TaskResponse taskResponse = taskService.updateTask(taskId, userId, taskRequest);
 		EntityModel<TaskResponse> entityModel = EntityModel.of(taskResponse);
 		entityModel.add(linkHateoas.linkGetTask(taskId, projectId));
 		return new ResponseEntity<>(
-				new DataResponse(HttpStatus.OK.value(), entityModel, "Update infomation request successful."),
-				HttpStatus.OK);
+				new DataResponse(HttpStatus.OK.value(), entityModel, "Update task request successful."), HttpStatus.OK);
 	}
 
-	@PutMapping("/task/{taskId}/commit_content")
-	public ResponseEntity<DataResponse> commitContent(@PathVariable Long projectId, @PathVariable Long taskId,
+	@PutMapping("/tasks/{taskId}/commit_content")
+	public ResponseEntity<DataResponse> commitContent(@PathVariable UUID projectId, @PathVariable UUID taskId,
 			@RequestBody TaskRequest taskRequest) {
 		TaskResponse taskResponse = taskService.commitContentTask(taskId, taskRequest);
 		EntityModel<TaskResponse> entityModel = EntityModel.of(taskResponse);
@@ -84,28 +84,48 @@ public class TaskController {
 				HttpStatus.OK);
 	}
 
-	@DeleteMapping("/users/{userId}/task/{taskId}")
-	public ResponseEntity<DataResponse> deleteTask(@PathVariable Long projectId, @PathVariable Long userId,
-			@PathVariable Long taskId) {
+	@DeleteMapping("/users/{userId}/tasks/{taskId}")
+	public ResponseEntity<DataResponse> deleteTask(@PathVariable UUID projectId, @PathVariable UUID userId,
+			@PathVariable UUID taskId) {
 		return new ResponseEntity<>(
 				new DataResponse(HttpStatus.OK.value(), taskService.deleteTask(userId, taskId), "Request Successful."),
 				HttpStatus.OK);
 	}
 
 	@GetMapping("/users/{userId}/tasks")
-	public ResponseEntity<PaginationResponse<EntityModel<TaskResponse>>> getTasksByUserIdAndProjectAndStatusAndDate(
-			@PathVariable Long userId, @PathVariable Long projectId,
-			@RequestParam(required = false, defaultValue = "PLANNING") String status, @RequestParam String date,
-			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
-		LocalDate dateConvert = new ConvertData().toDate(date);
-		Page<TaskResponse> taskPage = taskService.getTaskByUserIdAndProjectIdAndStatusAndDate(userId, projectId, status,
-				dateConvert, PageRequest.of(page, size));
+	public ResponseEntity<PaginationResponse<EntityModel<TaskResponse>>> getTasks(@PathVariable UUID userId,
+			@PathVariable UUID projectId, @RequestParam(required = false, defaultValue = "TODO") String status,
+			@RequestParam(defaultValue = "name") String sortField,
+			@RequestParam(defaultValue = "asc") String sortDirection, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size) {
+
+		Direction direction = Direction.fromString(sortDirection.toUpperCase());
+
+		Page<TaskResponse> taskPage = taskService.getTasks(userId, projectId, status,
+				PageRequest.of(page, size, Sort.by(direction, sortField)));
+
 		List<EntityModel<TaskResponse>> entityModels = taskPage.getContent().stream()
 				.map(t -> EntityModel.of(t).add(linkHateoas.linkGetTask(projectId, t.getId()))).toList();
+
 		PaginationResponse<EntityModel<TaskResponse>> paginationResponse = new PaginationResponse<>(HttpStatus.OK,
 				entityModels, taskPage.getPageable().getPageNumber(), taskPage.getSize(), taskPage.getTotalElements(),
 				taskPage.getTotalPages(), taskPage.getSort().isSorted(), taskPage.getSort().isUnsorted(),
 				taskPage.getSort().isEmpty());
+
 		return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
 	}
+
+	@GetMapping("/users/{userId}/tasks/GDP_task_status")
+	public ResponseEntity<DataResponse> getGDPTaskByStatus(@PathVariable UUID userId, @PathVariable UUID projectId) {
+		String gdpTask = taskService.getGDPTask(userId, projectId);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), null, gdpTask), HttpStatus.OK);
+	}
+
+	@PutMapping("/users/{userId}/tasks/{taskId}/toUser/{toUserId}")
+	public ResponseEntity<DataResponse> asignTaskToUser(@PathVariable UUID projectId, @PathVariable UUID userId,
+			@PathVariable UUID taskId, @PathVariable UUID toUserId) {
+		taskService.assignToUser(taskId, userId, toUserId);
+		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), null, "Successfull"), HttpStatus.OK);
+	}
+
 }
