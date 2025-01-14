7f6372c9d5cc754fe05dbb4b7d3dff5660dee341
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:33:41 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/specification/ProjectSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/ProjectSpecification.java
@@ -1,12 +1,15 @@
 package com.quocbao.projectmanager.specification;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import com.quocbao.projectmanager.entity.Project;
 import com.quocbao.projectmanager.entity.Project_;
 import com.quocbao.projectmanager.entity.Task;
 import com.quocbao.projectmanager.entity.Task_;
-import com.quocbao.projectmanager.entity.User;
+import com.quocbao.projectmanager.entity.User_;
 
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Subquery;
@@ -17,26 +20,25 @@ public class ProjectSpecification {
 
 	}
 
-	public static Specification<Project> findByUserId(User user) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Project_.user), user);
+	public static Specification<Project> findByUserId(UUID user) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Project_.user).get(User_.id), user);
 	}
 
 	public static Specification<Project> findByStatus(String status) {
 		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Project_.status), status);
 	}
 
-	public static Specification<Project> findProject(User user) {
+	public static Specification<Project> findProject(UUID user) {
 		return (root, query, criteriaBuilder) -> {
-			@SuppressWarnings("null")
-			Subquery<Long> subQuery = query.subquery(Long.class);
+			Subquery<UUID> subQuery = query.subquery(UUID.class);
 			Root<Task> rootTask = subQuery.from(Task.class);
 			subQuery.select(rootTask.get(Task_.project).get(Project_.id));
-			subQuery.where(criteriaBuilder.equal(rootTask.get(Task_.user), user));
+			subQuery.where(criteriaBuilder.equal(rootTask.get(Task_.user).get(User_.id), user));
 			return criteriaBuilder.in(root.get(Project_.id)).value(subQuery);
 		};
 	}
 
-	public static Specification<Project> search(String keySearch) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Project_.name), keySearch);
+	public static Specification<Project> byDate(Timestamp start, Timestamp end) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get(Project_.dateEnd), start, end);
 	}
 }
