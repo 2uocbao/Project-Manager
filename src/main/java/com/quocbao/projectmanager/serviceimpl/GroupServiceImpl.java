e44ddafd095e439d40e590e4561f50af8ad334a6
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:17:34 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/serviceimpl/GroupServiceImpl.java
+++ src/main/java/com/quocbao/projectmanager/serviceimpl/GroupServiceImpl.java
@@ -1,132 +1,151 @@
 package com.quocbao.projectmanager.serviceimpl;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.quocbao.projectmanager.entity.Group;
-import com.quocbao.projectmanager.entity.Role;
+import com.quocbao.projectmanager.entity.Member;
+import com.quocbao.projectmanager.entity.Member.Role;
 import com.quocbao.projectmanager.entity.User;
 import com.quocbao.projectmanager.exception.ResourceNotFoundException;
+import com.quocbao.projectmanager.payload.response.GroupResponse;
 import com.quocbao.projectmanager.payload.response.UserResponse;
 import com.quocbao.projectmanager.repository.GroupRepository;
-import com.quocbao.projectmanager.repository.RoleRepository;
-import com.quocbao.projectmanager.repository.UserRepository;
+import com.quocbao.projectmanager.repository.MemberRepository;
 import com.quocbao.projectmanager.service.GroupService;
-import com.quocbao.projectmanager.specification.GroupSpecification;
+import com.quocbao.projectmanager.specification.MemberSpecification;
+
+import jakarta.persistence.Tuple;
+import jakarta.transaction.Transactional;
 
 @Service
 public class GroupServiceImpl implements GroupService {
 
 	private GroupRepository groupRepository;
-	private UserRepository userRepository;
-	private RoleRepository roleRepository;
+	private MemberRepository memberRepository;
 
-	public GroupServiceImpl(GroupRepository groupRepository, UserRepository userRepository,
-			RoleRepository roleRepository) {
+	public GroupServiceImpl(GroupRepository groupRepository, MemberRepository memberRepository) {
 		this.groupRepository = groupRepository;
-		this.userRepository = userRepository;
-		this.roleRepository = roleRepository;
+		this.memberRepository = memberRepository;
 	}
 
 	@Override
-	public Group createGroup(Long userId, String name) {
-		User user = userRepository.findById(userId).get();
-		Group group = new Group();
-		group.setName(name);
-		group.setUser(user);
-		Role role = roleRepository.findById(3L).get();
-		if (!user.getRoles().contains(role)) {
-			user.getRoles().add(role);
-			userRepository.save(user);
-		}
+	@Transactional
+	public Group createGroup(UUID userId, UUID withUser) {
+		Set<Member> members = new HashSet<>();
+		Group group = groupRepository.save(new Group("null", members));
+		members.add(new Member(group.getId(), userId, Role.ADMIN));
+		members.add(new Member(group.getId(), withUser, Role.ADMIN));
 		return groupRepository.save(group);
 	}
 
+	// Optimize, JPASpecificationExcuter
 	@Override
-	public Page<Group> getGroupsByUserId(Long userId, Pageable pageable, String keySearch) {
-		Specification<Group> specification;
-		if (keySearch != null && !keySearch.isEmpty()) {
-			specification = GroupSpecification.getGroupsByUserId(User.builder().id(userId).build())
-					.and(GroupSpecification.search(keySearch));
-		} else {
-			specification = GroupSpecification.getGroupsByUserId(User.builder().id(userId).build());
+	public Page<GroupResponse> getGroupsByUserId(UUID userId, Pageable pageable, String keySearch) {
+		Page<Tuple> result = groupRepository.getGroupsOfUser(userId.toString(), pageable);
+		return result.map(
+				t -> new GroupResponse((String) t.get(0), (String) t.get(1), (String) t.get(2), (Timestamp) t.get(3)));
+	}
+
+	@Override
+	public Group updateGroup(UUID userId, UUID groupId, String name) {
+		Specification<Member> specification;
+		List<UUID> groupIds = new ArrayList<>();
+		groupIds.add(groupId);
+		specification = MemberSpecification.findMemberByGroupId(groupIds)
+				.and(MemberSpecification.findMemberByUserId(userId));
+		Member member = memberRepository.findOne(specification).get();
+		if (member.getRole().equals(Role.ADMIN)) {
+			Group group = groupRepository.findById(groupId)
+					.orElseThrow(() -> new ResourceNotFoundException("Request failed."));
+			group.setName(name);
+			return groupRepository.save(group);
 		}
-		return groupRepository.findAll(specification, pageable);
+		return null;
+
 	}
 
 	@Override
-	public Group updateGroup(Long userId, Long groupId, String name) {
-		return groupRepository.findById(groupId).map(t -> {
-			if (t.getUser().getId().equals(userId)) {
-				t.setName(name);
-			}
-			return groupRepository.save(t);
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed."));
-	}
-
-	@Override
-	public String deleteGroup(Long userId, Long groupId) {
-		return groupRepository.findById(groupId).map(t -> {
-			if (t.getUser().getId().equals(userId)) {
-				groupRepository.delete(t);
-			}
+	public String deleteGroup(UUID userId, UUID groupId) {
+		Specification<Member> specification;
+		List<UUID> groupIds = new ArrayList<>();
+		groupIds.add(groupId);
+		specification = MemberSpecification.findMemberByGroupId(groupIds)
+				.and(MemberSpecification.findMemberByUserId(userId));
+		Member member = memberRepository.findOne(specification).get();
+		if (member.getRole().equals(Role.ADMIN)) {
+			Group group = groupRepository.findById(groupId)
+					.orElseThrow(() -> new ResourceNotFoundException("Request failed."));
+			groupRepository.delete(group);
 			return "Successful";
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed."));
+		}
+		return null;
 	}
 
 	@Override
-	public String addUserToGroup(Long userId, Long groupId) {
-		return groupRepository.findById(groupId).map(t -> {
-			if (!t.getUsers().contains(User.builder().id(userId).build())) {
-				t.getUsers().add(User.builder().id(userId).build());
-				groupRepository.save(t);
+	public String addUserToGroup(UUID userId, UUID memberId, UUID groupId) {
+		Specification<Member> specification;
+		List<UUID> groupIds = new ArrayList<>();
+		groupIds.add(groupId);
+		specification = MemberSpecification.findMemberByGroupId(groupIds)
+				.and(MemberSpecification.findMemberByUserId(userId));
+		Member member = memberRepository.findOne(specification).get();
+		if (member.getRole().equals(Role.ADMIN)) {
+			specification = MemberSpecification.findMemberByGroupId(groupIds)
+					.and(MemberSpecification.findMemberByUserId(memberId));
+			if (!memberRepository.findOne(specification).isEmpty()) {
+				return "ok";
 			}
+			memberRepository.save(new Member(groupId, memberId, Role.MEMBER));
 			return "Successful";
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed."));
+		}
+		return "Request failed";
 	}
 
 	@Override
-	public String removeUserInGroup(Long userId, Long groupId) {
-		return groupRepository.findById(groupId).map(t -> {
-			if (t.getUsers().contains(User.builder().id(userId).build())) {
-				t.getUsers().remove(User.builder().id(userId).build());
-				groupRepository.save(t);
-			}
+	public String removeUserInGroup(UUID userId, UUID memberId, UUID groupId) {
+		Specification<Member> specification;
+		List<UUID> groupIds = new ArrayList<>();
+		groupIds.add(groupId);
+		specification = MemberSpecification.findMemberByGroupId(groupIds)
+				.and(MemberSpecification.findMemberByUserId(userId));
+		Member member = memberRepository.findOne(specification).get();
+		if (member.getRole().equals(Role.ADMIN)) {
+			memberRepository.delete(MemberSpecification.findMemberByUserId(memberId));
 			return "Successful";
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed."));
+		}
+		return "Request failed";
 	}
 
 	@Override
-	public Page<UserResponse> getUsersInGroup(Long groupId, Pageable pageable, String keySearch) {
-		return groupRepository.findById(groupId).map(group -> {
-			// Get the list of users in the group
-			List<User> users;
-
-			if (keySearch != null && !keySearch.isEmpty()) {
-				users = group.getUsers().stream().filter(t -> t.getFirstName().contains(keySearch))
-						.filter(t -> t.getLastName().contains(keySearch)).toList();
-			} else {
-				users = group.getUsers();
-			}
-			// Convert List<User> to List<UserResponse>
-			List<UserResponse> userResponses = users.stream().map(UserResponse::new).toList();
-
-			// Manually handle pagination
-			int start = (int) pageable.getOffset();
-			int end = Math.min((start + pageable.getPageSize()), userResponses.size());
-
-			if (start > end) {
-				return new PageImpl<UserResponse>(new ArrayList<>(), pageable, userResponses.size());
-			}
-
-			List<UserResponse> paginatedList = userResponses.subList(start, end);
-			return new PageImpl<>(paginatedList, pageable, userResponses.size());
-		}).orElseThrow(() -> new ResourceNotFoundException("Group not found with id: " + groupId));
+	public Page<UserResponse> getUsersInGroup(UUID groupId, Pageable pageable, String keySearch) {
+		Specification<Member> specification;
+		List<UUID> groupIds = new ArrayList<>();
+		groupIds.add(groupId);
+		specification = MemberSpecification.findMemberByGroupId(groupIds);
+		Page<Member> members = memberRepository.findAll(specification, pageable);
+		Page<User> users = members.map(Member::getUser);
+		return users.map(UserResponse::new);
 	}
+
+	@Override
+	public Page<UserResponse> getUsersConnect(UUID userId, Pageable pageable, String keySearch) {
+		Specification<Member> specification = MemberSpecification.findMemberByUserId(userId);
+		Page<Member> members = memberRepository.findAll(specification, pageable);
+		List<UUID> groupIds = members.stream().map(Member::getGroup).map(Group::getId).distinct().toList();
+		Specification<Member> groupMembersSpecification = MemberSpecification.findMemberByGroupId(groupIds)
+				.and(MemberSpecification.excludeUserId(userId));
+		Page<Member> groupMembers = memberRepository.findAll(groupMembersSpecification, pageable);
+		return groupMembers.map(t -> new UserResponse(t.getUser()));
+	}
+
 }
