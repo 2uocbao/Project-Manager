720e31f1eb83be6bdd28319a876eb0974c4faee6
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:33:57 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/specification/TaskSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/TaskSpecification.java
@@ -1,13 +1,17 @@
 package com.quocbao.projectmanager.specification;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.data.jpa.domain.Specification;
 
-import com.quocbao.projectmanager.entity.Project;
+import com.quocbao.projectmanager.entity.Project_;
 import com.quocbao.projectmanager.entity.Task;
 import com.quocbao.projectmanager.entity.Task_;
-import com.quocbao.projectmanager.entity.User;
+import com.quocbao.projectmanager.entity.User_;
+
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 
 public class TaskSpecification {
 
@@ -15,12 +19,22 @@ public class TaskSpecification {
 
 	}
 
-	public static Specification<Task> getTaskByUserId(User user) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.user), user);
+	public static Specification<Task> getTaskDetailCustom(UUID taskId) {
+		return (root, query, criteriaBuilder) -> {
+			root.fetch(Task_.user);
+			query.multiselect(root.get(Task_.user).get(User_.firstName),
+					root.get(Task_.user).get(User_.firstName), root.get(Task_.user).get(User_.id));
+			return criteriaBuilder.equal(root.get(Task_.id), taskId);
+		};
 	}
 
-	public static Specification<Task> getTaskByProjectId(Project project) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.project), project);
+	public static Specification<Task> getTaskByUserId(UUID user) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.user).get(User_.id), user);
+	}
+
+	public static Specification<Task> getTaskByProjectId(UUID project) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.project).get(Project_.id),
+				project);
 	}
 
 	public static Specification<Task> getTaskByStatus(String status) {
@@ -31,7 +45,4 @@ public class TaskSpecification {
 		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Task_.type), type);
 	}
 
-	public static Specification<Task> getTaskByDate(LocalDate dateEnd) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get(Task_.dateEnd), dateEnd);
-	}
 }
