bf3a860ddb6496b8f2e00e2a9906e27692c5185a
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:24:38 +0700

    Update handling in methods

--- src/main/java/com/quocbao/projectmanager/serviceimpl/ProjectServiceImpl.java
+++ src/main/java/com/quocbao/projectmanager/serviceimpl/ProjectServiceImpl.java
@@ -1,15 +1,19 @@
 package com.quocbao.projectmanager.serviceimpl;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import com.quocbao.projectmanager.common.ConvertData;
 import com.quocbao.projectmanager.common.MethodGeneral;
-import com.quocbao.projectmanager.elasticsearch.entity.ProjectES;
-import com.quocbao.projectmanager.elasticsearch.repository.ProjectESRepository;
+import com.quocbao.projectmanager.common.StatusEnum;
+//import com.quocbao.projectmanager.elasticsearch.entity.ProjectES;
+//import com.quocbao.projectmanager.elasticsearch.repository.ProjectESRepository;
 import com.quocbao.projectmanager.entity.Project;
 import com.quocbao.projectmanager.entity.Role;
 import com.quocbao.projectmanager.entity.User;
@@ -26,29 +30,104 @@ import com.quocbao.projectmanager.specification.ProjectSpecification;
 public class ProjectServiceImpl implements ProjectService {
 
 	private ProjectRepository projectRepository;
-	private ProjectESRepository projectESRepository;
+//	private ProjectESRepository projectESRepository;
 	private UserRepository userRepository;
 	private RoleRepository roleRepository;
 	private MethodGeneral methodGeneral;
 
 	public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository,
-			RoleRepository roleRepository, MethodGeneral methodGeneral, ProjectESRepository projectESRepository) {
+			RoleRepository roleRepository, MethodGeneral methodGeneral
+//			, ProjectESRepository projectESRepository
+	) {
 		this.projectRepository = projectRepository;
-		this.projectESRepository = projectESRepository;
+//		this.projectESRepository = projectESRepository;
 		this.userRepository = userRepository;
 		this.roleRepository = roleRepository;
 		this.methodGeneral = methodGeneral;
 	}
 
 	@Override
-	public ProjectResponse createProject(Long userId, ProjectRequest projectRequest) {
+	public ProjectResponse createProject(UUID userId, ProjectRequest projectRequest) {
 		User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 		Project project = new Project(projectRequest);
 		project.setUser(user);
 		assignRoleIfNotPresent(user);
 		project = projectRepository.save(project);
-		projectESRepository.save(new ProjectES(project));
+//		projectESRepository.save(new ProjectES(project));
 		return new ProjectResponse(project);
+	}
+
+	@Override
+	public ProjectResponse getProject(UUID userId, UUID projectId) {
+		return new ProjectResponse(retrieveProject(projectId));
+	}
+
+	@Override
+	public ProjectResponse updateProject(UUID userId, UUID projectId, ProjectRequest projectRequest) {
+		return projectRepository.findById(projectId).map(p -> {
+//			methodGeneral.validatePermission(userId, p.getUser().getId());
+			p = p.updateProject(projectRequest);
+			projectRepository.save(p);
+//			updateProjectES(p);
+			return new ProjectResponse(p);
+		}).orElseThrow(() -> new ResourceNotFoundException("Project not found"));
+	}
+
+	@Override
+	public String deleteProject(UUID userId, UUID projectId) {
+		Project project = retrieveProject(projectId);
+		methodGeneral.validatePermission(userId, project.getUser().getId());
+		projectRepository.delete(project);
+//		deleteProjectES(projectId);
+		return "Delete Successfully";
+	}
+
+	@Override
+	public Page<ProjectResponse> getProjects(UUID userId, String status, String startDate, String endDate,
+			Pageable pageable) {
+		Timestamp start = ConvertData.toTimestamp(startDate);
+		Timestamp end = ConvertData.toTimestamp(endDate);
+		Specification<Project> spec;
+
+		spec = Specification.where(ProjectSpecification.findByUserId(userId))
+				.or(ProjectSpecification.findProject(userId)).and(ProjectSpecification.findByStatus(status))
+				.and(ProjectSpecification.byDate(start, end));
+
+		return projectRepository.findAll(spec, pageable).map(ProjectResponse::new);
+	}
+
+	@Override
+	public String getGDPProjectStatus(UUID userId, String startDate, String endDate) {
+		Timestamp start = ConvertData.toTimestamp(startDate);
+		Timestamp end = ConvertData.toTimestamp(endDate);
+		Specification<Project> spec;
+		spec = byStatus(userId, StatusEnum.DONE).and(ProjectSpecification.byDate(start, end));
+		long done = projectRepository.count(spec);
+		spec = byStatus(userId, StatusEnum.PENDING).and(ProjectSpecification.byDate(start, end));
+		long pending = projectRepository.count(spec);
+		spec = byStatus(userId, StatusEnum.INPROGRESS).and(ProjectSpecification.byDate(start, end));
+		long todo = projectRepository.count(spec);
+
+		return "Done: " + done + "pending: " + pending + "todo: " + todo;
+	}
+
+	private Specification<Project> byStatus(UUID userId, StatusEnum status) {
+		return Specification.where(ProjectSpecification.findByUserId(userId))
+				.or(ProjectSpecification.findProject(userId)).and(ProjectSpecification.findByStatus(status.toString()));
+	}
+//	@Override
+//	public Page<String> findByUserIdAndStatusAndKeySearch(UUID userId, String status, String keySearch,
+//			Pageable pageable) {
+//		return null;
+//		if(keySearch == null || keySearch.isEmpty()) {
+//			return projectESRepository.findByUserIdAndStatusAndPageable(userId.toString(), status, pageable);
+//		}
+//		return projectESRepository.findByUserIdAndStatusAndPageable(userId.toString(), status, keySearch, pageable);
+//	}
+
+	private Project retrieveProject(UUID projectId) {
+		return projectRepository.findById(projectId)
+				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
 	}
 
 	private void assignRoleIfNotPresent(User user) {
@@ -60,65 +139,13 @@ public class ProjectServiceImpl implements ProjectService {
 		}
 	}
 
-	@Override
-	public ProjectResponse getProject(Long userId, Long projectId) {
-		return projectRepository.findById(projectId).map(ProjectResponse::new)
-				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-	}
+//	private void updateProjectES(Project project) {
+//	ProjectES projectES = projectESRepository.findById(project.getId().toString()).get();
+//	projectESRepository.save(projectES.updateProjectES(project));
+//}
 
-	@Override
-	public ProjectResponse updateProject(Long userId, Long projectId, ProjectRequest projectRequest) {
-		return projectRepository.findById(projectId).map(p -> {
-			methodGeneral.validatePermission(userId, p.getUser().getId());
-			p = p.updateProject(projectRequest);
-			projectRepository.save(p);
-			updateProjectES(p);
-			return new ProjectResponse(p);
-		}).orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-	}
-
-	private void updateProjectES(Project project) {
-		ProjectES projectES = projectESRepository.findById(project.getId().toString()).get();
-		projectESRepository.save(projectES.updateProjectES(project));
-	}
-
-	@Override
-	public String deleteProject(Long userId, Long projectId) {
-		Project project = projectRepository.findById(projectId)
-				.orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-		methodGeneral.validatePermission(userId, project.getUser().getId());
-		projectRepository.delete(project);
-		deleteProjectES(projectId);
-		return "Delete Successfully";
-	}
-
-	private void deleteProjectES(Long projectId) {
-		ProjectES projectES = projectESRepository.findById(projectId.toString()).get();
-		projectESRepository.delete(projectES);
-	}
-
-	@Override
-	public Page<ProjectResponse> getProjectsByUserIdAndStatus(Long userId, String status, String keySearch,
-			Pageable pageable) {
-		Specification<Project> spec;
-		if (keySearch != null && !keySearch.isEmpty()) {
-			spec = Specification.where(ProjectSpecification.findByUserId(User.builder().id(userId).build()))
-					.or(ProjectSpecification.findProject(User.builder().id(userId).build()))
-					.and(ProjectSpecification.findByStatus(status)).and(ProjectSpecification.search(keySearch));
-		} else {
-			spec = Specification.where(ProjectSpecification.findByUserId(User.builder().id(userId).build()))
-					.or(ProjectSpecification.findProject(User.builder().id(userId).build()))
-					.and(ProjectSpecification.findByStatus(status));
-		}
-		return projectRepository.findAll(spec, pageable).map(ProjectResponse::new);
-	}
-
-	@Override
-	public Page<ProjectES> findByUserIdAndStatusAndKeySearch(Long userId, String status, String keySearch,
-			Pageable pageable) {
-		if(keySearch == null || keySearch.isEmpty()) {
-			return projectESRepository.findByUserIdAndStatusAndPageable(userId.toString(), status, pageable);
-		}
-		return projectESRepository.findByUserIdAndStatusAndPageable(userId.toString(), status, keySearch, pageable);
-	}
+//	private void deleteProjectES(UUID projectId) {
+//	ProjectES projectES = projectESRepository.findById(projectId.toString()).get();
+//	projectESRepository.delete(projectES);
+//}
 }
