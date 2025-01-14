7fcb31ac8a229f519160d35f08619f29a7f504e9
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:34:22 +0700

    Update

--- src/main/java/com/quocbao/projectmanager/specification/UserSpecification.java
+++ src/main/java/com/quocbao/projectmanager/specification/UserSpecification.java
@@ -1,15 +1,19 @@
 package com.quocbao.projectmanager.specification;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.domain.Specification;
 
-import com.quocbao.projectmanager.entity.Friend;
-import com.quocbao.projectmanager.entity.Friend_;
-import com.quocbao.projectmanager.entity.Group;
 import com.quocbao.projectmanager.entity.Group_;
+import com.quocbao.projectmanager.entity.Member;
+import com.quocbao.projectmanager.entity.Member_;
+import com.quocbao.projectmanager.entity.Message_;
 import com.quocbao.projectmanager.entity.User;
 import com.quocbao.projectmanager.entity.User_;
 
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Subquery;
 import jakarta.persistence.metamodel.SingularAttribute;
@@ -20,25 +24,40 @@ public class UserSpecification {
 
 	}
 
+	public static Specification<User> searchUser(SingularAttribute<User, String> column, String value) {
+		return (root, query, criteriaBulider) -> criteriaBulider.like(root.get(column), value);
+	}
+
 	public static Specification<User> findByColumn(SingularAttribute<User, String> column, String value) {
 		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(column), value);
 	}
 
-	public static Specification<User> propertiesByUserId(Long userId) {
-		return (root, query, criteriaBuilder) -> {
-			@SuppressWarnings("null")
-			Subquery<Long> subquery = query.subquery(Long.class);
-			Root<Friend> rootFriend = subquery.from(Friend.class);
-			subquery.select(rootFriend.get(Friend_.friendId));
-			subquery.where(criteriaBuilder.equal(rootFriend.get(Friend_.userId), userId));
-			return criteriaBuilder.in(root.get(User_.id)).value(subquery);
-		};
+	public static Specification<User> excludeUserId(UUID userId) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get(User_.id), userId);
 	}
 
-	public static Specification<User> getUsersInGroup(Long groupId) {
+	public static Specification<User> searchUser(UUID userId, Boolean isFind) {
 		return (root, query, criteriaBuilder) -> {
-			Join<User, Group> member = root.join(User_.groups);
-			return criteriaBuilder.equal(member.get(Group_.id), groupId);
+			
+			Subquery<UUID> groupSubquery = query.subquery(UUID.class);
+			Root<Member> groupRoot = groupSubquery.from(Member.class);
+			
+			Subquery<UUID> userGroupSubquery = query.subquery(UUID.class);
+			Root<Member> userGroupRoot = userGroupSubquery.from(Member.class);
+			
+			// query select group ID by userId
+			userGroupSubquery.select(userGroupRoot.get(Member_.group).get(Group_.id))
+					.where(criteriaBuilder.equal(userGroupRoot.get(Member_.user).get(User_.id), userId));
+			
+			// query select user in group ID
+			groupSubquery.select(groupRoot.get(Member_.user).get(User_.id))
+					.where(groupRoot.get(Member_.group).get(Group_.id).in(userGroupSubquery));
+			
+			// case for find users with the same group
+			if(Boolean.TRUE.equals(isFind)) {
+				return criteriaBuilder.and(root.get(User_.id).in(groupSubquery));
+			}
+			return criteriaBuilder.not(root.get(User_.id).in(groupSubquery));
 		};
 	}
 }
