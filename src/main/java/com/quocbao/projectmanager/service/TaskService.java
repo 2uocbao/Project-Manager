3fb1ffcdc0680d84321861e4b8aa44ccfdc2256d
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:10:56 +0700

    Change type response and parameters

--- src/main/java/com/quocbao/projectmanager/service/TaskService.java
+++ src/main/java/com/quocbao/projectmanager/service/TaskService.java
@@ -1,26 +1,29 @@
 package com.quocbao.projectmanager.service;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.quocbao.projectmanager.entity.Task;
 import com.quocbao.projectmanager.payload.request.TaskRequest;
 import com.quocbao.projectmanager.payload.response.TaskResponse;
 
 public interface TaskService {
 
-	public TaskResponse createTask(Long projectId, TaskRequest taskRequest);
+	public TaskResponse createTask(UUID projectId, TaskRequest taskRequest);
 
-	public Task getTask(Long taskId);
+	public TaskResponse getTask(UUID taskId);
 
-	public TaskResponse updateTask(Long taskId, Long userId, TaskRequest taskRequest);
+	public TaskResponse updateTask(UUID taskId, UUID userId, TaskRequest taskRequest);
 
-	public TaskResponse commitContentTask(Long taskId, TaskRequest taskRequest);
+	public TaskResponse commitContentTask(UUID taskId, TaskRequest taskRequest);
 
-	public String deleteTask(Long userId, Long taskId);
+	public String deleteTask(UUID userId, UUID taskId);
 
-	public Page<TaskResponse> getTaskByUserIdAndProjectIdAndStatusAndDate(Long userId, Long projectId, String status,
-			LocalDate date, Pageable pageable);
+	public Page<TaskResponse> getTasks(UUID userId, UUID projectId, String status,
+			Pageable pageable);
+	
+	public String getGDPTask(UUID userId, UUID projectId);
+	
+	public void assignToUser(UUID taskId, UUID userId, UUID toUserId);
 }
