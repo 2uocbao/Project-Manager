16b1c12d306aabc3495e5d06b9c04dc9f959fa9c
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 10:53:41 +0700

    Update method

--- src/main/java/com/quocbao/projectmanager/controller/NotificationController.java
+++ src/main/java/com/quocbao/projectmanager/controller/NotificationController.java
@@ -1,21 +1,25 @@
 package com.quocbao.projectmanager.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quocbao.projectmanager.common.DataResponse;
-import com.quocbao.projectmanager.entity.Notification;
-import com.quocbao.projectmanager.payload.request.FriendRequest;
+import com.quocbao.projectmanager.common.PaginationResponse;
+import com.quocbao.projectmanager.payload.request.NotifiRequest;
+import com.quocbao.projectmanager.payload.response.NotifiResponse;
 import com.quocbao.projectmanager.websocket.PushNotificationService;
 
 @RestController
@@ -29,20 +33,24 @@ public class NotificationController {
 	}
 
 	@GetMapping("/notifications")
-	public ResponseEntity<DataResponse> getNotificationsByUserId(@PathVariable Long userId) {
-		List<Notification> notifications = notificationService.getNotificationsByUserId(userId);
-		return new ResponseEntity<>(
-				new DataResponse(HttpStatus.OK.value(), notifications, "Retrieve notifications by userId successful."),
-				HttpStatus.OK);
+	public ResponseEntity<PaginationResponse<EntityModel<NotifiResponse>>> getNotificationsByUserId(
+			@PathVariable UUID userId, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size) {
+		Page<NotifiResponse> notifications = notificationService.getNotificationsByUserId(userId,
+				PageRequest.of(page, size));
+		List<EntityModel<NotifiResponse>> entityModels = notifications.getContent().stream().map(EntityModel::of)
+				.toList();
+		PaginationResponse<EntityModel<NotifiResponse>> paginationResponse = new PaginationResponse<>(HttpStatus.OK,
+				entityModels, notifications.getPageable().getPageNumber(), notifications.getSize(),
+				notifications.getTotalElements(), notifications.getTotalPages(), notifications.getSort().isSorted(),
+				notifications.getSort().isUnsorted(), notifications.getSort().isEmpty());
+
+		return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
 	}
 
-	@MessageMapping("/friend.request")
-	@SendTo("/topic/{userId}/friend-request")
-	public ResponseEntity<DataResponse> friendRequest(@DestinationVariable Long userId,
-			@Payload FriendRequest friendRequest) {
-		notificationService.pushNotification(userId, "/topic/" + userId + "/friend-request",
-				"You have friend request from " + friendRequest.getFromUserName() + " " + friendRequest.getFromUser(),
-				"Friend Request");
+	@MessageMapping("/notifi")
+	public ResponseEntity<DataResponse> notifiRequest(@Payload NotifiRequest notifiRequest) {
+		notificationService.pushNotification(notifiRequest);
 		return new ResponseEntity<>(new DataResponse(HttpStatus.OK.value(), null, "Successful"), HttpStatus.OK);
 	}
 }
