013225c2558e9ef9a494c720c801ae3da45f8486
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 13:49:28 +0700

    Add method

--- src/main/java/com/quocbao/projectmanager/repository/GroupRepository.java
+++ src/main/java/com/quocbao/projectmanager/repository/GroupRepository.java
@@ -1,12 +1,52 @@
 package com.quocbao.projectmanager.repository;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.quocbao.projectmanager.entity.Group;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.Tuple;
+
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long>, JpaSpecificationExecutor<Group> {
+public interface GroupRepository extends JpaRepository<Group, UUID>, JpaSpecificationExecutor<Group> {
+
+	@Query(value = """
+			SELECT
+			    HEX(gm.id) AS uuid_str,
+			    (
+			        SELECT GROUP_CONCAT(u2.first_name SEPARATOR ' ')
+			        FROM user u2
+			        WHERE u2.id = (
+			            SELECT mg.user_id
+			            FROM member_group mg, group_member gm
+			            WHERE mg.user_id != u.id
+			        )
+			    ) AS first_names,
+			    (
+			        SELECT m.message
+			        FROM message m, group_member gm
+			        WHERE m.group_id = gm.id
+			        ORDER BY m.created_at DESC
+			        LIMIT 1
+			    ) AS last_message,
+			    (
+			        SELECT m.created_at
+			        FROM message m, group_member gm
+			        WHERE m.group_id = gm.id
+			        ORDER BY m.created_at DESC
+			        LIMIT 1
+			    ) AS last_message_time
+			FROM user u, group_member gm
+			WHERE u.id = UNHEX(REPLACE(:userId, '-', ''))
+			""", nativeQuery = true)
+
+	Page<Tuple> getGroupsOfUser(@Param("userId") String userId, Pageable pageable);
 
 }
