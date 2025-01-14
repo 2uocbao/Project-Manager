5814647233ed31b967ff1ec372aaaa330bfc1df9
2uocbao2116 <2uocbao.IT@gmail.com>
2025-01-14 14:07:53 +0700

    Change type response and parameters

--- src/main/java/com/quocbao/projectmanager/service/GroupService.java
+++ src/main/java/com/quocbao/projectmanager/service/GroupService.java
@@ -1,24 +1,29 @@
 package com.quocbao.projectmanager.service;
 
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.quocbao.projectmanager.entity.Group;
+import com.quocbao.projectmanager.payload.response.GroupResponse;
 import com.quocbao.projectmanager.payload.response.UserResponse;
 
 public interface GroupService {
 
-	public Group createGroup(Long userId, String name);
+	public Group createGroup(UUID userId, UUID withUser);
 
-	public Page<Group> getGroupsByUserId(Long userId, Pageable pageable, String keySearch);
+	public Page<GroupResponse> getGroupsByUserId(UUID userId, Pageable pageable, String keySearch);
 
-	public Group updateGroup(Long userId, Long groupId, String name);
+	public Group updateGroup(UUID userId, UUID groupId, String name);
 
-	public String deleteGroup(Long userId, Long groupId);
+	public String deleteGroup(UUID userId, UUID groupId);
 
-	public String addUserToGroup(Long userId, Long groupId);
+	public String addUserToGroup(UUID userId, UUID memberId, UUID groupId);
 
-	public String removeUserInGroup(Long userId, Long groupId);
+	public String removeUserInGroup(UUID userId, UUID memberId, UUID groupId);
 
-	public Page<UserResponse> getUsersInGroup(Long groupId, Pageable pageable, String keySearch);
+	public Page<UserResponse> getUsersInGroup(UUID groupId, Pageable pageable, String keySearch);
+	
+	public Page<UserResponse> getUsersConnect(UUID userId, Pageable pageable, String keySearch);
 }
