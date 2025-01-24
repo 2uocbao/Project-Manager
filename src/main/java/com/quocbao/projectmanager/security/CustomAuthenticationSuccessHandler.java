549acdb36d1554815157462eb5d4c48ad55b65a3
Quốc Bảo <2uocbao.it@gmail.com>
2025-01-24 23:05:44 +0700

    Update handle

--- src/main/java/com/quocbao/projectmanager/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/quocbao/projectmanager/security/CustomAuthenticationSuccessHandler.java
@@ -9,11 +9,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.quocbao.projectmanager.payload.request.LoginRequest;
+import com.quocbao.projectmanager.entity.User;
 import com.quocbao.projectmanager.payload.request.UserRequest;
-import com.quocbao.projectmanager.payload.response.UserResponse;
+import com.quocbao.projectmanager.security.jwt.JwtTokenProvider;
+import com.quocbao.projectmanager.security.oauthconfig.CustomOauth2User;
 import com.quocbao.projectmanager.service.UserService;
 
 import jakarta.servlet.ServletException;
@@ -25,54 +24,38 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
 	private UserService userService;
 
-	public CustomAuthenticationSuccessHandler(UserService userService) {
+	private JwtTokenProvider jwtTokenProvider;
+
+	public CustomAuthenticationSuccessHandler(UserService userService, JwtTokenProvider jwtTokenProvider) {
 		this.userService = userService;
+		this.jwtTokenProvider = jwtTokenProvider;
 	}
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
+
 		if (authentication instanceof OAuth2AuthenticationToken) {
-
-			OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-			UserResponse userResponse;
-			ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-
-			String givenName = oAuth2User.getAttribute("given_name");
-			String familyName = oAuth2User.getAttribute("family_name");
-			String email = oAuth2User.getAttribute("email");
-			String sub = oAuth2User.getAttribute("sub");
-
-			UserDetails userDetails = userService.loadUserByUsername(email);
-
-			if (userDetails == null) {
-				UserRequest userRequest = new UserRequest();
-				userRequest.setFirstName(givenName);
-				userRequest.setLastName(familyName);
-				userRequest.setEmail(email);
-				userRequest.setPassword(sub);
-
-				userResponse = userService.registerUser(userRequest);
-
-				String json = objectWriter.writeValueAsString(userResponse);
-				response.setContentType("application/json");
-				response.setStatus(HttpServletResponse.SC_OK);
-				response.getWriter().write(json);
-
+			CustomOauth2User oAuth2User = new CustomOauth2User((OAuth2User) authentication.getPrincipal());
+			// Retrieve user detail in database
+			UserDetails userDetails = userService.loadUserByUsername(oAuth2User.getEmail());
+			String jwtToken = null;
+			// Check if user detail is null
+			if (userDetails != null) {
+				jwtToken = jwtTokenProvider.generateToken(userDetails);
 			} else {
-
-				LoginRequest loginRequest = new LoginRequest(sub, sub);
-				userResponse = userService.loginUser(loginRequest);
-				String json = objectWriter.writeValueAsString(userResponse);
-
-				response.setContentType("application/json");
-				response.setStatus(HttpServletResponse.SC_OK);
-				response.getWriter().write(json);
-
+				// Create new user if user does not exist
+				UserRequest userRequest = new UserRequest();
+				userRequest.setFirstName(oAuth2User.getAttribute("given_name"));
+				userRequest.setLastName(oAuth2User.getAttribute("family_name"));
+				userRequest.setEmail(oAuth2User.getAttribute("email"));
+				userRequest.setPassword(oAuth2User.getAttribute("sub"));
+				User user = userService.registerUser(userRequest);
+				jwtToken = jwtTokenProvider.generateToken(user.userDetails(user));
 			}
-
-			response.sendRedirect("myapp://oauth2redirect");
+			response.sendRedirect("/users/oauth2?email=" + oAuth2User.getEmail() + "&jwtToken=" + jwtToken);
 		}
+
 	}
 
 }
