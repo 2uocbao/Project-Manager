02034f89e09bcbd8d1f62f4cfe37d5ea60c72745
Quốc Bảo <2uocbao.it@gmail.com>
2025-01-24 23:02:07 +0700

    Extends Jpa Specification Excutor

--- src/main/java/com/quocbao/projectmanager/repository/RoleRepository.java
+++ src/main/java/com/quocbao/projectmanager/repository/RoleRepository.java
@@ -1,11 +1,12 @@
 package com.quocbao.projectmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.quocbao.projectmanager.entity.Role;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
 
 }
